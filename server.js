8fecfd660e06d44132a41f3520505564ca89b26e
Jncosd99 <jpasse@live.com>
2025-09-30 16:08:16 -0500

    Add MongoDB connection to server

--- server.js
+++ server.js
@@ -1,7 +1,14 @@
 require('dotenv').config();
 const express = require('express');
+const mongoose = require('mongoose');
 const app = express();
 const port = process.env.PORT || 5000;
+
+app.use(express.json());
+
+mongoose.connect(process.env.MONGODB_URI)
+  .then(() => console.log('Connected to MongoDB'))
+  .catch(err => console.error('MongoDB connection error:', err));
 
 app.get('/', (req, res) => {
   res.send('Envelope Accounts API');
